8830ebac88ab664d70ccb2f729543d283a6eaf69
JeffersonS69 <ibarra.jefferson1@gmail.com>
2025-01-07 21:05:38 -0500

    Subir proyecto base

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const logger = new Logger('Microservice User Story');
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Application is running on PORT ${envs.port}`);
+}
+bootstrap();
